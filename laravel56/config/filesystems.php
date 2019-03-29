28a39c5b60ee08601481d10c4b5aa1851c4d6f30
Ayuan97 <15201035366@163.com>
2019-03-29 11:54:32 +0800

    lrd1

--- laravel56/config/filesystems.php
+++ laravel56/config/filesystems.php
@@ -54,10 +54,7 @@ return [
             'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
         ],
-        'upload' => [
-            'driver' => 'local',
-            'root' => public_path('brandpic'),
-        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
@@ -68,4 +65,5 @@ return [
         ],
 
     ],
+
 ];
